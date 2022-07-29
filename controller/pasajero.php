450468670f683795db63f764d754bc1b24d1cc86
Milton Amador <miltonamador1508@gmail.com>
2022-07-29 17:15:15 -0600

    commit pasajeros

--- controller/pasajero.php
+++ controller/pasajero.php
@@ -1,5 +1,52 @@
 <?php
-if (condition) {
-    var hola;
-}
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+    header('Access-Control-Allow-Headers: token, Content-Type');
+    header('Access-Control-Max-Age: 1728000');
+    header('Content-Length: 0');
+    header('Content-Type: text/plain');
+    die();
+  }
+    header('Access-Control-Allow-Origin: *');  
+    header('Content-Type: application/json');
+
+    require_once("../config/conexion.php");
+    require_once("../models/pasajero.php");
+    
+    $pasajeros = new Pasajeros();
+    
+    $body = json_decode(file_get_contents("php://input"), true);
+
+    switch($_GET["opc"]){
+
+        case "GetPasajeros":
+            $datos=$pasajeros->get_pasajeros();
+            echo json_encode($datos);
+        break;
+        
+        case "GetPasajero":
+            $datos=$pasajeros->get_pasajero($body["CodigoPasajero"]);
+            echo json_encode($datos);
+        break;   
+        
+        case "InsertPasajero":
+            $datos=$pasajeros->insert_pasajero($body["CodigoPasajero"],$body["Nombres"],$body["Apellidos"],$body["Fecha_Registro"],$body["Nacionalidad"],$body["Numero_Telefonico"],$body["Email"]);
+            echo json_encode("Pasajero Agregado Correctamente");
+        break;
+
+        case "UpdatePasajero":
+            $datos=$pasajeros->update_pasajero($body["CodigoPasajero"],$body["Nombres"],$body["Apellidos"],$body["Fecha_Registro"],$body["Nacionalidad"],$body["Numero_Telefonico"],$body["Email"]);
+            echo json_encode("Pasajero Actualizado Correctamente");         
+        break;
+
+        case "DeletePasajero":
+            $datos=$pasajeros->delete_pasajero($body["CodigoPasajero"]);
+            echo json_encode("Pasajero Eliminado Correctamente");      
+        break;
+
+        
+    }
+
+
 ?>
--- models/pasajero.php
+++ models/pasajero.php
@@ -1,5 +1,66 @@
 <?php
-if (condition) {
-    var hola;
+ class Pasajeros extends Conectar{
+
+    public function get_pasajeros(){
+        $conectar= parent::conexion();
+        parent::set_names();
+        $sql="SELECT * FROM PASAJERO ";
+        $sql=$conectar->prepare($sql);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function get_pasajero($CodigoPasajero){
+        $conectar= parent::conexion();
+        parent::set_names();
+        $sql="SELECT * FROM pasajero WHERE CodigoPasajero = ?";
+        $sql=$conectar->prepare($sql);
+        $sql->bindValue(1, $CodigoPasajero);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function insert_pasajero($CodigoPasajero, $Nombres, $Apellidos, $Fecha_Registro, $Nacionalidad, $Numero_Telefonico, $Email){
+        $conectar= parent::conexion();
+        parent::set_names();
+        $sql="INSERT INTO pasajero(CodigoPasajero, Nombres, Apellidos, Fecha_Registro, Nacionalidad, Numero_Telefonico, Email)
+        VALUES (?,?,?,?,?,?,?);";
+        $sql=$conectar->prepare($sql);
+        $sql->bindValue(1, $CodigoPasajero);
+        $sql->bindValue(2, $Nombres);
+        $sql->bindValue(3, $Apellidos);
+        $sql->bindValue(4, $Fecha_Registro);
+        $sql->bindValue(5, $Nacionalidad);
+        $sql->bindValue(6, $Numero_Telefonico);
+        $sql->bindValue(7, $Email);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC); 
+    }
+
+    public function update_pasajero($CodigoPasajero, $Nombres, $Apellidos, $Fecha_Registro, $Nacionalidad, $Numero_Telefonico, $Email){
+        $conectar= parent::conexion();
+        parent::set_names();
+        $sql=" UPDATE pasajero SET Nombres= ?, Apellidos= ?, Fecha_Registro= ?, Nacionalidad= ?, Numero_Telefonico= ?, Email= ? WHERE CodigoPasajero= ?";
+        $sql=$conectar->prepare($sql);
+        $sql->bindValue(7, $CodigoPasajero);
+        $sql->bindValue(1, $Nombres);
+        $sql->bindValue(2, $Apellidos);
+        $sql->bindValue(3, $Fecha_Registro);
+        $sql->bindValue(4, $Nacionalidad);
+        $sql->bindValue(5, $Numero_Telefonico);
+        $sql->bindValue(6, $Email);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC); 
+    }
+     
+    public function delete_pasajero($CodigoPasajero){
+        $conectar= parent::conexion();
+        parent::set_names();
+        $sql=" DELETE FROM pasajero WHERE CodigoPasajero= ? ";
+        $sql=$conectar->prepare($sql);
+        $sql->bindValue(1, $CodigoPasajero);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC); 
+    }
 }
 ?>
