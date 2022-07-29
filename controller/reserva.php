2028708f477fa594142211a6ef9481f3a714f694
SaidArte <ivis.arteaga@unah.hn>
2022-07-29 17:04:48 -0600

    Commit Reserva

--- controller/reserva.php
+++ controller/reserva.php
@@ -1,5 +1,47 @@
 <?php
-if (condition) {
-    var hola;
-}
+     if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+        header('Access-Control-Allow-Headers: token, Content-Type');
+        header('Access-Control-Max-Age: 1728000');
+        header('Content-Length: 0');
+        header('Content-Type: text/plain');
+        die();
+      }
+        header('Access-Control-Allow-Origin: *');  
+        header('Content-Type: application/json');
+
+        require_once("../config/conexion.php");
+        require_once("../models/reserva.php");
+        $reserva = new Reserva();
+
+        $body  = json_decode(file_get_contents("php://input"), true);
+
+        switch($_GET["opc"]){
+            case "GetReservas":
+                $datos=$reserva->get_reservas();
+                echo json_encode($datos);
+            break;
+
+            case "GetReserva":
+                $datos=$reserva->get_reserva($body["idReservacion"]);
+                echo json_encode($datos);
+            break;
+
+            case "InsertReserva":
+                $datos=$reserva->insert_reserva($body["idReservacion"],$body["CodigoVuelo"],$body["CodigoPasajero"],$body["NombrePasajero"], $body["CiudadDestino"], $body["FechaVuelo"],$body["PrecioVuelo"]);
+                echo json_encode("Registro de Reservacion Ingresado Corectamente");
+            break;
+            case "UpdateReserva":
+                $datos=$reserva->update_reserva($body["idReservacion"],$body["CodigoVuelo"],$body["CodigoPasajero"],$body["NombrePasajero"], $body["CiudadDestino"], $body["FechaVuelo"],$body["PrecioVuelo"]);
+                echo json_encode("Registro de Reservacion Actualizado Corectamente");
+            break;
+            case "DeleteReserva":
+                $datos=$reserva->delete_reserva($body["idReservacion"]);
+                echo json_encode("Registro de Reservacion Eliminado Corectamente");
+            break;
+           
+
+
+        }
 ?>
--- models/reserva.php
+++ models/reserva.php
@@ -1,5 +1,75 @@
 <?php
-if (condition) {
-    var hola;
-}
+    class Reserva extends Conectar {
+
+        public function get_reservas(){
+            $conectar= parent::conexion();
+            parent:: set_names();
+            $sql="SELECT * FROM reserva "; 
+            $sql=$conectar->prepare($sql);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+
+
+        }
+        public function get_reserva($idReservacion){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="SELECT * FROM reserva WHERE IDRESERVACION = ?"; 
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $idReservacion);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+            
+
+        }
+        
+        public function insert_reserva ($idReservacion, $CodigoVuelo, $CodigoPasajero, $NombrePasajero, $CiudadDestino, $FechaVuelo, $PrecioVuelo){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="INSERT INTO reserva(idReservacion,CodigoVuelo,CodigoPasajero,NombrePasajero,CiudadDestino,FechaVuelo,PrecioVuelo)
+            VALUE (?,?,?,?,?,?,?);";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $idReservacion);
+            $sql->bindValue(2, $CodigoVuelo);
+            $sql->bindValue(3, $CodigoPasajero);
+            $sql->bindValue(4, $NombrePasajero);
+            $sql->bindValue(5, $CiudadDestino);
+            $sql->bindValue(6, $FechaVuelo);
+            $sql->bindValue(7, $PrecioVuelo);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+
+        }
+
+        public function update_reserva ($idReservacion, $CodigoVuelo, $CodigoPasajero, $NombrePasajero, $CiudadDestino, $FechaVuelo, $PrecioVuelo){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="UPDATE reserva SET CodigoVuelo= ?,CodigoPasajero= ?,NombrePasajero= ?,CiudadDestino= ?,FechaVuelo= ?,PrecioVuelo= ? WHERE idReservacion= ? ";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(7, $idReservacion);
+            $sql->bindValue(1, $CodigoVuelo);
+            $sql->bindValue(2, $CodigoPasajero);
+            $sql->bindValue(3, $NombrePasajero);
+            $sql->bindValue(4, $CiudadDestino);
+            $sql->bindValue(5, $FechaVuelo);
+            $sql->bindValue(6, $PrecioVuelo);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+
+        }
+
+        public function delete_reserva($idReservacion){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="DELETE FROM reserva WHERE idReservacion = ?"; 
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $idReservacion);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+            
+
+        }
+
+    
+    }
 ?>
