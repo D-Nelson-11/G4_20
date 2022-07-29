24cbdb388b36b46d382a608fac8e9be36f6f192b
D-Nelson-11 <david.nelson@unah.hn>
2022-07-29 17:21:48 -0600

    Commit Vuelo y Carga Final del Proyecto

--- controller/vuelo.php
+++ controller/vuelo.php
@@ -1,5 +1,58 @@
 <?php
-if (condition) {
-    var hola;
-}
+
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+    header('Access-Control-Allow-Headers: token, Content-Type');
+    header('Access-Control-Max-Age: 1728000');
+    header('Content-Length: 0');
+    header('Content-Type: text/plain');
+    die();
+  }
+    header('Access-Control-Allow-Origin: *');  
+    header('Content-Type: application/json');
+
+    require_once("../config/conexion.php");
+    require_once("../models/vuelo.php");
+    $vuelos= new vuelos();
+
+    $body = json_decode(file_get_contents("php://input"),true);
+
+    switch ($_GET["opc"]) {
+
+        case "GetVuelos":
+            $datos=$vuelos->get_vuelos();
+            echo json_encode($datos);
+        break;
+
+        case "GetVuelo":
+            $datos=$vuelos->get_vuelo($body["CodigoVuelo"]);
+            echo json_encode($datos);
+        break;
+        
+        case "InsertVuelo":
+            $datos=$vuelos->insert_vuelo($body["CodigoVuelo"],$body["CiudadOrigen"],$body["CiudadDestino"],$body["FechaVuelo"],
+                                         $body["CantidadPasajeros"],$body["TipoAvion"],$body["DistanciaKm"]);
+
+            echo json_encode("Registros de Vuelo Ingresados Correctamente");
+        break;
+
+        case "UpdateVuelo":
+            $datos=$vuelos->update_vuelo($body["CodigoVuelo"],$body["CiudadOrigen"],$body["CiudadDestino"],$body["FechaVuelo"],
+                                         $body["CantidadPasajeros"],$body["TipoAvion"],$body["DistanciaKm"]);
+                                         
+            echo json_encode("¡Registro Actualizado Correctamente!");
+        break;
+
+        
+        case "DeleteVuelo":
+            $datos=$vuelos->delete_vuelo($body["CodigoVuelo"]);
+                                         
+            echo json_encode("¡Registro Eliminado Correctamente!");
+        break;
+        
+        default:
+            # code...
+            break;
+    }
 ?>
--- models/vuelo.php
+++ models/vuelo.php
@@ -1,5 +1,70 @@
 <?php
-if (condition) {
-    var hola;
-}
+    class vuelos extends Conectar{
+
+        public function get_vuelos(){
+            $conectar = parent::conexion();
+            parent::set_names();
+            $sql=" SELECT * FROM vuelo ";
+            $sql=$conectar->prepare($sql);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function get_vuelo($CodigoVuelo){
+            $conectar = parent::conexion();
+            parent::set_names();
+            $sql=" SELECT * FROM vuelo WHERE CodigoVuelo= ? ";
+            $sql=$conectar->prepare($sql);
+            $sql-> bindValue(1,$CodigoVuelo);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function insert_vuelo($CodigoVuelo, $CiudadOrigen, $CiudadDestino, $FechaVuelo, $CantidadPasajeros, $TipoAvion, $DistanciaKm){
+            $conectar = parent::conexion();
+            parent::set_names();
+            $sql=" INSERT INTO vuelo (CodigoVuelo,CiudadOrigen,CiudadDestino,FechaVuelo,CantidadPasajeros,TipoAvion,DistanciaKm) 
+            VALUES(?,?,?,?,?,?,?)";
+            $sql=$conectar->prepare($sql);
+            $sql-> bindValue(1,$CodigoVuelo);
+            $sql-> bindValue(2,$CiudadOrigen);
+            $sql-> bindValue(3,$CiudadDestino);
+            $sql-> bindValue(4,$FechaVuelo);
+            $sql-> bindValue(5,$CantidadPasajeros);
+            $sql-> bindValue(6,$TipoAvion);
+            $sql-> bindValue(7,$DistanciaKm);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+
+        }
+
+        public function update_vuelo($CodigoVuelo, $CiudadOrigen, $CiudadDestino, $FechaVuelo, $CantidadPasajeros, $TipoAvion, $DistanciaKm){
+            $conectar = parent::conexion();
+            parent::set_names();
+            $sql=" UPDATE vuelo SET CiudadOrigen= ?,CiudadDestino= ?,FechaVuelo= ?,CantidadPasajeros= ?,TipoAvion= ?,DistanciaKm= ? WHERE CodigoVuelo= ? ";
+            $sql=$conectar->prepare($sql);
+            $sql-> bindValue(7,$CodigoVuelo);
+            $sql-> bindValue(1,$CiudadOrigen);
+            $sql-> bindValue(2,$CiudadDestino);
+            $sql-> bindValue(3,$FechaVuelo);
+            $sql-> bindValue(4,$CantidadPasajeros);
+            $sql-> bindValue(5,$TipoAvion);
+            $sql-> bindValue(6,$DistanciaKm);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+
+        }
+
+        public function delete_vuelo($CodigoVuelo){
+            $conectar = parent::conexion();
+            parent::set_names();
+            $sql=" DELETE FROM vuelo WHERE CodigoVuelo= ? ";
+            $sql=$conectar->prepare($sql);
+            $sql-> bindValue(1,$CodigoVuelo);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+
+        }
+    }
 ?>
+
