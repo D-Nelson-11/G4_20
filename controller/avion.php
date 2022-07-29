3122b6ab456c8dc0650ef79a7135e6450d7df6af
NilssonCastellon <nilsson.castellon@unah.hn>
2022-07-29 16:46:44 -0600

    Commit Avion

--- controller/avion.php
+++ controller/avion.php
@@ -1,5 +1,48 @@
 <?php
-if (condition) {
-    var hola;
+if($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+    header('Access-Control-Allow-Headers: token, Content-Type');
+    header('Access-Control-Max-Age: 1728000');
+    header('Content-Length: 0');
+    header('Content-Type: text/plain');
+    die();
 }
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+require_once("../config/conexion.php");
+require_once("../models/avion.php");
+$aviones = new Aviones();
+
+$body = json_decode(file_get_contents("php://input"), true);
+
+switch($_GET["opc"]){
+    case "GetAviones":
+        $datos=$aviones->get_aviones();
+        echo json_encode($datos);
+    break;
+
+    case "GetAvion":
+        $datos=$aviones->get_avion($body["NumeroAvion"]);
+        echo json_encode($datos);
+    break;
+
+    case "InsertAvion":
+        $datos=$aviones->insert_avion($body["NumeroAvion"],$body["TipoAvion"],$body["HorasVuelo"],$body["CapacidadPasajeros"],$body["FechaPrimerVuelo"],$body["PaisConstruccion"],$body["CantidadVuelos"]);
+        echo json_encode("¡Avion Agregado!");
+    break;
+    
+    case "DeleteAvion":
+        $datos=$aviones->delete_avion($body["NumeroAvion"]);
+        echo json_encode("¡Avion Eliminado!");
+    break;
+
+    case "UpdateAvion":
+        $datos=$aviones->update_Avion($body["NumeroAvion"],$body["TipoAvion"],$body["HorasVuelo"],$body["CapacidadPasajeros"],$body["FechaPrimerVuelo"],$body["PaisConstruccion"],$body["CantidadVuelos"]);
+        echo json_encode("¡Avion Actualizado!");
+    break;
+}
+
 ?>
--- models/avion.php
+++ models/avion.php
@@ -1,5 +1,68 @@
 <?php
-if (condition) {
-    var hola;
-}
+
+    class Aviones extends Conectar{
+
+      public function get_aviones(){
+        $conectar= parent::conexion();
+        parent::set_names();
+        $sql=" SELECT * FROM avion ";
+        $sql=$conectar->prepare($sql);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+      }
+
+      public function get_avion($NumeroAvion){
+        $conectar= parent::conexion();
+        parent::set_names();
+        $sql=" SELECT * FROM avion WHERE NumeroAvion = ? ";
+        $sql=$conectar->prepare($sql);
+        $sql->bindValue(1, $NumeroAvion);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+     }
+
+      public function insert_avion($NumeroAvion, $TipoAvion, $HorasVuelo, $CapacidadPasajeros, $FechaPrimerVuelo, $PaisConstruccion, $CantidadVuelos){
+        $conectar= parent::conexion();
+        parent::set_names();
+        $sql="INSERT INTO avion(NumeroAvion, TipoAvion, HorasVuelo, CapacidadPasajeros, FechaPrimerVuelo, PaisConstruccion, CantidadVuelos)
+        VALUES (?,?,?,?,?,?,?);";
+        $sql=$conectar->prepare($sql);
+        $sql->bindValue(1, $NumeroAvion);
+        $sql->bindValue(2, $TipoAvion);
+        $sql->bindValue(3, $HorasVuelo);
+        $sql->bindValue(4, $CapacidadPasajeros);
+        $sql->bindValue(5, $FechaPrimerVuelo);
+        $sql->bindValue(6, $PaisConstruccion);
+        $sql->bindValue(7, $CantidadVuelos);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+      }
+      
+      public function delete_avion($NumeroAvion){
+        $conectar= parent::conexion();
+        parent::set_names();
+        $sql="DELETE FROM avion WHERE NumeroAvion = ?";
+        $sql=$conectar->prepare($sql);
+        $sql->bindValue(1, $NumeroAvion);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+     }
+
+     public function update_avion($NumeroAvion, $TipoAvion, $HorasVuelo, $CapacidadPasajeros, $FechaPrimerVuelo, $PaisConstruccion, $CantidadVuelos){
+        $conectar= parent::conexion();
+        parent::set_names();
+        $sql="UPDATE avion SET TipoAvion = ?, HorasVuelo = ?, CapacidadPasajeros = ?, FechaPrimerVuelo = ?, PaisConstruccion = ?, CantidadVuelos= ? WHERE NumeroAvion= ? ";
+       
+        $sql=$conectar->prepare($sql);
+        $sql->bindValue(1, $TipoAvion);
+        $sql->bindValue(2, $HorasVuelo);
+        $sql->bindValue(3, $CapacidadPasajeros);
+        $sql->bindValue(4, $FechaPrimerVuelo);
+        $sql->bindValue(5, $PaisConstruccion);
+        $sql->bindValue(6, $CantidadVuelos);
+        $sql->bindValue(7, $NumeroAvion);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+      }
+   }
 ?>
